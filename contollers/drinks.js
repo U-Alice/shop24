e253a09ab1b3265d4d1357c0161e29dced5958ca
u-Alice <umugwanezaalice22@gmail.com>
2022-08-17 13:08:47 +0200

    completed get most consumed drinks by ID

--- contollers/drinks.js
+++ contollers/drinks.js
@@ -12,6 +12,16 @@ module.export.getAllDrinks = ()=>{
 
 module.exports.mostConsumed  = ()=>{
     return async (req, res)=>{
-        let query = `SELECT * from drinks order desc`
+        let query = `SELECT * from drinks order desc limit 5`;
+        const drinks = connection.query(query, (err)=>{
+            if (err) throw err
+        })
+        res.json({message: 'Retrieved most consumed drinks', drinks: drinks}).status(200)
+
+    }
+}
+module.exports.getById = ()=>{
+    return async (req, res)=>{
+        let
     }
 }
