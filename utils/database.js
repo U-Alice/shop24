5fb651da6db71a1f6a5da4981af987fc66c88073
u-Alice <umugwanezaalice22@gmail.com>
2022-08-24 10:06:22 +0200

    changed db configuration

--- utils/database.js
+++ utils/database.js
@@ -4,8 +4,8 @@ module.exports.connection = mysql.createConnection({
     host: 'localhost', 
     user: 'root', 
     port: '3306',
-    password: '@MysqlRoot22', 
-    database: 'Shop24'
+    password: '', 
+    database: 'shop24'
 })
 
 
